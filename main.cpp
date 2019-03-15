5ded7fa9ce7e135a42d5dc2a0e9e2cb84a1f7c60
kjs1005 <48340259+kjs1005@users.noreply.github.com>
2019-03-15 20:27:51 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main(int argc, char** argv) 
+{
+	int year,month,day;
+	cout<<"請輸入年";
+	cin>>year;
+	cout<<"請輸入月";
+	cin>>month;
+	cout<<"請輸入日";
+	cin>>day;
+	cout<<"出生年月日為"<<year<<"年"<<month<<"月"<<day<<"日";
+	return 0;
+}
